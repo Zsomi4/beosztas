ee0ae5bdddef469716cffadbb8c69e70cdef0639
Zsomi <k72302872172@ktch.hu>
2022-05-31 08:41:15 +0200

    A ComboBox rárakása a GUI menüre.

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -2,27 +2,34 @@ package views;
 
 import java.awt.FlowLayout;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
     JPanel mainPanel;
+    public DefaultComboBoxModel<String> groupModel;
+    public JComboBox<String> groupCombo;
     public MainFrame() {
         this.setMainComponent();
         this.setMainPanel();
         this.setMainFrame();
     }
     private void setMainComponent() {
-        
+        this.groupModel = new DefaultComboBoxModel<>();
+        this.groupCombo = new JComboBox<>(this.groupModel);
     }
     private void setMainPanel() {
         this.mainPanel = new JPanel();
+        this.mainPanel.add(this.groupCombo);
     }
     private void setMainFrame() {
         this.add(this.mainPanel);
         this.setLayout(new FlowLayout());
+        this.setTitle("Beosztások");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(300, 400);
+        this.setSize(400, 300);
         this.setVisible(true);
     }
 }
